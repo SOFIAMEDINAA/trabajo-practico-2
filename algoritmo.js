161d63fa5a2f725616005c63136b7b2b9ffe7e17
Sofia Medina <medinasofiaa1@gmail.com>
2023-10-22 12:05:41 -0300

    actualizacion LocalStore

--- algoritmo.js
+++ algoritmo.js
@@ -1,47 +1,60 @@
 
-const loginForm = document.querySelector("#login-form");
-const registroUsuario = document.querySelector("#usuario");
+
 const errorUsuario = document.querySelector("#mensaje-error")
 
-//registro y almacenamiento localStore//
 
-const usuarios = "usuarios";
-const datosRegistro = "nombre y apellido";
+function registrarUsuario(nombreUsuario, email,) {
+    const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    usuariosRegistrados.push({ nombreUsuario, email });
 
-let listaUsuarios = "lista vacia";
-listaUsuarios = [];
-
-let listaDatos = "lista vacia";
-listaDatos = [];
-
-function registrarUsuario() {
-
-    let registroNombre = document.querySelector("#nombre").value;
-    let registroApellido = document.querySelector("#apellido").value;
-    let usuarioRegistrado = registroUsuario.value;
-
-    listaDatos.push(registroApellido, registroNombre);
-    listaUsuarios.push(usuarioRegistrado);
-    localStorage.setItem(datosRegistro, JSON.stringify(listaDatos));
-    localStorage.setItem(usuarios, JSON.stringify(listaUsuarios));
+    localStorage.setItem('usuarios', JSON.stringify(usuariosRegistrados));
 
 }
+document.getElementById('registroForm'),('submit', function (e) {
+    e.preventDefault();
+   
+    const nombreUsuario = document.querySelector("#nombreUsuario").value;
+    const email = document.querySelector("#email").value;
+    registrarUsuario( nombreUsuario, email);
+
+    alert("registro exitoso");
+    this.reset();
+});
 
 
+document.getElementById('inicioSesionForm'),('submit', function(e) {
+    e.preventDefault();
 
-// Obtener el nombre de usuario almacenado en el localStorage
- function logearUsuario() {
-    
-    const usuarioLogin = document.querySelector("#ingresar-usuario").value;
-     let localStoreName = localStorage.getItem("usuarios"); 
+const usuarioInicioSesion= document.querySelector("#usuarioInicioSesion").value;
+const emailInicioSesion = document.querySelector("#emailInicioSesion").value;
 
-    if ((localStoreName === usuarioLogin)) {
-        errorUsuario.innerHTML= "bienvenido";
-    } else {
-        /* errorUsuario.innerHTML = "Usuario incorrecto"; */
-        alert("incorrecto");
-    }
+const usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+
+const usuarioEncontrado = usuariosRegistrados.find(user => user.nombreUsuario === usuarioInicioSesion && user.email === emailInicioSesion); 
+
+if (usuarioEncontrado) {
+    alert('Inicio de sesión exitoso');
+} else {
+    alert('Usuario o correo electrónico incorrectos');
 }
- 
+
+this.reset();
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
